e7e0232726d47ae214e8d9e6c8124896f337eb07
Jroslav85 <jroslav85@gmal.com>
2024-06-19 20:15:47 +0500

    управленние игроком

--- core/src/com/jroslav/game/Player.java
+++ core/src/com/jroslav/game/Player.java
@@ -1,15 +1,89 @@
 package com.jroslav.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
 
-	public Vector2 vectorPlayer;
+	public Vector2 positionPlayer;
 	public Sprite spritePlayer;
-	
+	private int speed = 250;
+	private float angle = 0f;
+
 	public Player(Texture img) {
 		spritePlayer = new Sprite(img);
+		positionPlayer = new Vector2(Gdx.graphics.getWidth() / 2,
+				spritePlayer.getHeight() / 2);
 	}
+
+	public void draw(SpriteBatch batch) {
+		update(Gdx.graphics.getDeltaTime());
+		spritePlayer.setPosition(positionPlayer.x, positionPlayer.y);
+		spritePlayer.draw(batch);
+	}
+
+	public void update(float deltaTime) {
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		if (Gdx.input.isKeyPressed(Keys.W)) {
+			if (spritePlayer.getRotation() == 0f || spritePlayer.getRotation() == 360f) {
+				positionPlayer.y += deltaTime * speed;
+			}
+			if (spritePlayer.getRotation() == 90f) {
+				positionPlayer.x -= deltaTime * speed;
+			}
+			if (spritePlayer.getRotation() == 180f) {
+				positionPlayer.y -= deltaTime * speed;
+			}
+			if (spritePlayer.getRotation() == 270f) {
+				positionPlayer.x += deltaTime * speed;
+			}
+		}
+		if (Gdx.input.isKeyPressed(Keys.S)) {
+			if (spritePlayer.getRotation() == 0f || spritePlayer.getRotation() == 360f) {
+				positionPlayer.y -= deltaTime * speed;
+			}
+			if (spritePlayer.getRotation() == 90f) {
+				positionPlayer.x += deltaTime * speed;
+			}
+			if (spritePlayer.getRotation() == 180f) {
+				positionPlayer.y += deltaTime * speed;
+			}
+			if (spritePlayer.getRotation() == 270f) {
+				positionPlayer.x -= deltaTime * speed;
+			}
+		}
+
+		if (Gdx.input.isKeyPressed(Keys.A)) {
+			if (spritePlayer.getRotation() == 360f) {
+				spritePlayer.setRotation(0f);
+			}
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			spritePlayer.rotate(90f);
+		}
+		if (Gdx.input.isKeyPressed(Keys.D)) {
+			if (spritePlayer.getRotation() == 0f) {
+				spritePlayer.setRotation(360f);
+			}
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			spritePlayer.rotate(-90f);
+		}
+		System.out.println(spritePlayer.getRotation());
+	}
+
 }
--- core/src/com/jroslav/game/TanksGame.java
+++ core/src/com/jroslav/game/TanksGame.java
@@ -1,35 +1,46 @@
 package com.jroslav.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class TanksGame extends ApplicationAdapter {
-	SpriteBatch batch;
 	Texture imgPlayer;
 	Player player;
-	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		createPlayer();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 640, 480);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(0.1f, 0.1f, 0, 1);
+	public void render() {
+		ScreenUtils.clear(0.1f, 0.1f, 0.01f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		
 		batch.begin();
-		batch.draw(imgPlayer, 0, 0);
+		player.draw(batch);
 		batch.end();
+		   
+
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		imgPlayer.dispose();
 	}
-	
+
 	private void createPlayer() {
 		imgPlayer = new Texture("player.png");
 		player = new Player(imgPlayer);
